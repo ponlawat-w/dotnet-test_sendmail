2c40bd95461aa020bebe77051f554cfc4f255243
ponlawat-w <ponlawat_w@cmu.ac.th>
2019-02-04 13:47:39 +0700

    Initial Commit

--- SmtpTest/Program.cs
+++ SmtpTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace SmtpTest
 {
@@ -6,7 +9,38 @@ namespace SmtpTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Host: ");
+            string host = Console.ReadLine();
+            Console.Write("SenderEmail: ");
+            string senderEmail = Console.ReadLine();
+            Console.Write("SenderPassword: ");
+            string senderPassword = Console.ReadLine();
+            Console.Write("Receiver: ");
+            string receiver = Console.ReadLine();
+
+            SmtpClient client = new SmtpClient(host)
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(senderEmail, senderPassword)
+            };
+
+            MailMessage message = new MailMessage(senderEmail, receiver)
+            {
+                Subject = "Hello World!",
+                Body = "This is a test email :)"
+            };
+
+            try
+            {
+                client.Send(message);
+                Console.WriteLine("Sent!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
